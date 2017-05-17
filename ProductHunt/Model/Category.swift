d337c9e0063fb12442b4918237a62cadad770c0a
Mihail Babaev <mmbabaev@gmail.com>
2017-05-18 01:42:00 +0300

    add first controller

---
+++ ProductHunt/Model/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Info.swift
+//  ProductHunt
+//
+//  Created by Mihail Babaev on 18.05.17.
+//  Copyright © 2017 Mihail Babaev. All rights reserved.
+//
+
+import Foundation
+
+struct Category {
+    let name: String
+    let colorCode: String
+}
---
+++ ProductHunt/View/DropDownCatigoriesMenu.swift
@@ -0,0 +1,16 @@
+//
+//  DropDownMenu.swift
+//  ProductHunt
+//
+//  Created by Mihail Babaev on 18.05.17.
+//  Copyright © 2017 Mihail Babaev. All rights reserved.
+//
+
+import UIKit
+import BTNavigationDropdownMenu
+
+class DropDownMenu: BTNavigationDropdownMenu {
+
+    item
+
+}
